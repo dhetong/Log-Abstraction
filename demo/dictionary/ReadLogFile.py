c7d485a7d96e52ae82f93f20a3b017f5003fa2ca
HetongD <dhetong0714@gmail.com>
2019-02-04 14:41:25 -0500

    A log abstraction method without source code

---
+++ demo/dictionary/ReadLogFile.py
@@ -0,0 +1,10 @@
+def tokenSplit(logLine):
+    pass;
+
+def readFile(fileName):
+    logFile = open(fileName, 'r');
+    logLine = logFile.readline();
+    pass;
+
+def dictionarySetUp(logFileName, DictionaryFile):
+    pass;
