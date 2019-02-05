4d227745a049e8086c28788e72fb072a4bdf2401
HetongD <dhetong0714@gmail.com>
2019-02-05 16:45:11 -0500

    2019.2.5

--- demo/dictionary/ReadLogFile.py
+++ demo/dictionary/ReadLogFile.py
@@ -1,10 +1,15 @@
-def tokenSplit(logLine):
-    pass;
+from demo.common.Common import tokenGenerator;
 
-def readFile(fileName):
-    logFile = open(fileName, 'r');
-    logLine = logFile.readline();
+def tokenFind(token, DictionaryFile):
     pass;
 
 def dictionarySetUp(logFileName, DictionaryFile):
+    logFile = open(logFileName, 'r');
+    dictionary = open(DictionaryFile, 'w');
+    while 1:
+        logLines = logFile.readline(1000000);
+        if not logLines:
+            break;
+        for line in logLines:
+            tokens = tokenGenerator(line);
     pass;
---
+++ demo/parsing/MatchLine.py
@@ -0,0 +1,44 @@
+from demo.common.Common import tokenGenerator;
+
+#the function which is used to detect dynamic and static parts based on single token
+def singleTokenCompare(logTokens, tokenFile, threshold, outputFile):
+    for token in logTokens:
+        while 1:
+            tokenList = tokenFile.readlines(100000);
+            findIdentifier = False;
+            if not tokenList:
+                break;
+            for tokenItem in tokenList:
+                if token in tokenItem:
+                    findIdentifier = True;
+                    tmp = tokenItem.split(',');
+                    frequency = int(tmp[1]);
+                    if frequency >= threshold:
+                        pass;
+                    else:
+                        token = '#'
+                    break;
+            if findIdentifier:
+                break;
+
+def tokenMatch(inputLogFile, outputEventFile, tokenFile):
+    #open the input log files, output log event files and the token dictionary
+    try:
+        inFile = open(inputLogFile, 'r');
+        outFile = open(outputEventFile, 'w');
+        tokenFile = open(tokenFile, 'r');
+
+        while 1:
+            logLines = inFile.readlines(100000);
+            if not logLines:
+                break;
+            for logLine in logLines:
+                logTokens = tokenGenerator(logLine);
+                pass;
+
+    finally:
+        if inFile and outFile and tokenFile:
+            inFile.close();
+            outFile.close();
+            tokenFile.close();
+    pass;
