dfde984ac4255b9c440c79c10f4342d1b0ba84fb
HetongD <dhetong0714@gmail.com>
2019-02-14 16:10:37 -0500

    fix dictionary establish

--- Main.py
+++ Main.py
@@ -1,4 +1,10 @@
 from demo.dictionary.ReadLogFile import dictionarySingleSetUp;
 from demo.dictionary.ReadLogFile import dictionaryDoubleSetUp;
 
-dictionarySingleSetUp('demo/Hadoop_2k.log','demo/SingleDictionary');
+dictionarySingleSetUp('Hadoop_2k.txt','SingleDictionary.txt');
+dictionaryDoubleSetUp('Hadoop_2k.txt','DoubleDictionary.txt');
+
+#FTest = open('Hadoop_2k.txt','r');
+#Lines = FTest.readlines(1000000);
+#for line in Lines:
+#   print(line);
--- demo/common/Common.py
+++ demo/common/Common.py
@@ -2,12 +2,30 @@ import re;
 
 # a function which change the split word to space
 def uniSplitWord(logLine):
-    tmp = re.sub(r'-_\(\)\[\]', " ", logLine);
+    #tmp = re.sub(r'-_,\(\)\[\]', " ", logLine);
+    tmp = re.sub(r'/(\d{4}[-/]\d{1,2}[-/]\d{1,2})','',logLine);
+    tmp = re.sub(r'(\d{4}[-/]\d{1,2}[-/]\d{1,2}\s\d{1,2}:\d{1,2})','',tmp);
+    tmp = re.sub(r'-',' ',tmp);
+    tmp = re.sub(r'_',' ',tmp);
+    tmp = re.sub(r',',' ',tmp);
+    tmp = re.sub(r'\[',' ',tmp);
+    tmp = re.sub(r'\]',' ',tmp);
+    tmp = re.sub(r'\(',' ',tmp);
+    tmp = re.sub(r'\)',' ',tmp);
+    tmp = re.sub(r'\{',' ',tmp);
+    tmp = re.sub(r'\}',' ',tmp);
+    tmp = re.sub(r':',' ',tmp);
+    tmp = re.sub(r';',' ',tmp);
+    tmp = re.sub(r'\.',' ',tmp);
+    tmp = re.sub(r'\/',' ',tmp);
+    tmp = re.sub(r'[0-9]*','',tmp)
     return tmp;
 
 # a function which is used to modify the log lines
 def tokenGenerator(logLine):
+    #print(logLine);
     logLineTmp = uniSplitWord(logLine);
+    #print(logLineTmp);
     tokenList = logLineTmp.split( );
     return tokenList;
 
