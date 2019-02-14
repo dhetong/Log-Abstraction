c1dccbdb69cd7cfe850634436410065828a5e613
HetongD <dhetong0714@gmail.com>
2019-02-14 16:07:48 -0500

    fix dictionary establish

---
+++ Main.py
@@ -0,0 +1,4 @@
+from demo.dictionary.ReadLogFile import dictionarySingleSetUp;
+from demo.dictionary.ReadLogFile import dictionaryDoubleSetUp;
+
+dictionarySingleSetUp('demo/Hadoop_2k.log','demo/SingleDictionary');
