24b10fadeeab54c9417ded92806a8180fbdc62a0
Hetong Dai <hetongdai@mehrans-mini.wireless.concordia.ca>
2019-06-10 13:55:18 -0400

    some updates

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from flask import Flask
 from flask import Blueprint
 main = Flask(__name__)
 
-import json
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
 from demo.dictionary.ReadLogFile import dictionarySingleSetUp;
 from demo.parsing.MatchLine import tokenMatchSingle;
 from demo.dictionary.ReadLogFile import dictionaryDoubleSetUp;
@@ -14,30 +16,34 @@ from demo.dictionary.ReadLogFile import dictionaryTripleSetUp;
 from demo.parsing.MatchLine import tokenMatchTriple;
 
 
-@main.route("/logs", methods=["GET"])
-def run_Singledictionary():
-    dictionarySingleSetUp('Zookeeper.log','SingleDictionaryzoo.txt');
+#@main.route("/Downloads", methods=["GET","POST"])
+#def run_Singledictionary():
+dictionarySingleSetUp('Zookeeper.log','SingleDictionaryzoo.txt');
 
-@main.route("/logs", methods=["GET"])
-def run_doubledictionary():
-    dictionaryDoubleSetUp('Zookeeper.log','DoubleDictionaryzoo.txt');
+#@main.route("/Downloads", methods=["GET","POST"])
+#def run_doubledictionary():
+dictionaryDoubleSetUp('Zookeeper.log','DoubleDictionaryzoo.txt');
 
 
-@main.route("/logs", methods=["GET"])
-def run_tripledictionary():
-    dictionaryTripleSetUp('Zookeeper.log','TripleDictionaryzoo.txt');
+#@main.route("/Downloads", methods=["GET","POST"])
+#def run_tripledictionary():
+dictionaryTripleSetUp('Zookeeper.log','TripleDictionaryzoo.txt');
 
 #@main.route("/", methods = ["POST"])
 #    def run_MatchSingle():
-    tokenMatchSingle('Zookeeper.log','LogEventSingle.txt','SingleDictionaryzoo.txt',3);
+tokenMatchSingle('Zookeeper.log','LogEventSingle.txt','SingleDictionaryzoo.txt',3);
 
 #@main.route("/", methods = ["POST"])
 #    def run_MatchDouble():
-    tokenMatchDouble('Zookeeper.log','LogEventDouble.txt','DoubleDictionaryzoo.txt',2);
+tokenMatchDouble('Zookeeper.log','LogEventDouble.txt','DoubleDictionaryzoo.txt',2);
 
 #@main.route("/", methods = ["POST"])
 #    def run_MatchTriple():
-    tokenMatchTriple('AZookeeper.log','logEventTriple.txt','TripleDictionaryzoo.txt','DoubleDictionaryzoo.txt',2,3);
 
-if __name__ == '__main__':
-    main.run(host= 'schen@192.168.165.202')
+sc = tokenMatchTriple('Zookeeper.log','logEventTriplezoo.txt','TripleDictionaryzoo.txt','DoubleDictionaryzoo.txt',2,3);
+ssc = StreamingContext(sc, 2)
+lines = ssc.socketTextStream("localhost", 9999)
+
+if __name__ == "__main__":
+
+    main.run(host='0.0.0.0',debug=True)
